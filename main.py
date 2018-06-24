6a7dfd63d57ca8630c2318df0c6c750ccec48089
Yogesh Pawar <yogeshpawar159@gmail.com>
2018-06-24 14:45:59 +0530

    Concurrent futures  applied for parallelism

--- main.py
+++ main.py
@@ -3,18 +3,13 @@ from xmljson import parker
 from json import dumps
 import argparse
 from xml.etree.ElementTree import fromstring
-# TODO: Use multiprocessing package to spawning multiple process_folder
-from multiprocessing import Pool
-from multiprocessing import Process
 import glob
+import concurrent.futures
 
 parser = argparse.ArgumentParser(description='Convert XML to JSON')
 parser.add_argument('-i', '--input_xml_folder', help='Path to the XML folder', default='./xml')
 parser.add_argument('-o', '--output_json_folder', help='Path to the JSON folder', default='./json')
 parser.add_argument('-max', '--max_threads', type=int, default=60)
-
-
-# TODO: Use this variable to run multiple threads of 'transform_xml_to_json'
 
 
 def transform_xml_to_json(xml_file_input, json_file_output):
@@ -32,19 +27,32 @@ def process_folder(xml_folder, json_folder):
     """For each file in xml_folder call transform_xml_to_json and
     Create JSON file in json_folder"""
     # TODO: To complete
-
-
-if __name__ == "__main__":
-    # TODO: Write a code to run transform_xml_to_json in multiple threads for different folders.
-    # This code should read the structure of input_xml_folder and replicate the structure in json_output_folder
-    # The code should use multiprocessing to spawn a thread for each sub folder up to MAX_THREADS
-    args = parser.parse_args()
-    print(args)
-    for name in glob.iglob('{}/**/*.xml'.format(args.input_xml_folder), recursive=True):
+    for name in glob.iglob('{}/**/*.xml'.format(xml_folder), recursive=True):
         print('input file', name)
         # Change file name extension
         oname = os.path.splitext(name)[0] + '.json'
         # Create new file path
-        print('output file', os.path.join(args.output_json_folder, oname[len(args.input_xml_folder) + 1:]))
-        print()
-print('done')
+        ofile = os.path.join(json_folder, oname[len(xml_folder) + 1:])
+        print('output file', ofile)
+        transform_xml_to_json(name, ofile)
+
+
+def worker(name):
+    print('input file', name)
+    # Change file name extension
+    oname = os.path.splitext(name)[0] + '.json'
+    # Create new file path
+    print(os.path.split(name))
+    ofile = os.path.join(args.output_json_folder, oname[len(args.input_xml_folder) + 1:])
+    print('output file', ofile)
+    transform_xml_to_json(name, ofile)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    print(args)
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=args.max_threads) as executor:
+        input_files = glob.iglob('{}/**/*.xml'.format(args.input_xml_folder), recursive=True)
+        executor.map(worker, input_files)
+    print('done')
