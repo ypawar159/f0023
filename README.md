bf081663cc59028db953004bb2c9fea2491d4721
Amir Aryani <amir.aryani@gmail.com>
2018-06-24 17:10:03 +1000

    template code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# f0023
-Transforming XML to JSON
+# Transforming XML to JSON
+The objective of this program is to read XML files from an XML folder, and
+transform them to JSON. This program is partially developed.
+To complete this program please fork the repository, complete the code and
+send a pull request. 
+
+Requirements are:
+
+* **Requirement 1. Replicate folder structure:** This program should replicate the input_xml_folder structure to create a similar structure 
+for the JSON files. Example of this is presented in Sample folder.
+* **Requirement 2. Multiprocessing:** This program should process folders using `process_folder` separate threads up to the
+MAX_THREADS identifier as in input parameter.
+
+ 
---
+++ main.py
@@ -0,0 +1,40 @@
+from xmljson import parker    
+from json import dumps
+import argparse
+from xml.etree.ElementTree import fromstring
+# TODO: Use multiprocessing package to spawning multiple process_folder
+from multiprocessing import Pool
+from multiprocessing import Process
+
+parser = argparse.ArgumentParser(description='Convert XML to JSON')
+parser.add_argument('-i', '--input_xml_folder', help='Path to the XML folder', default='./xml')
+parser.add_argument('-o', '--output_json_folder', help='Path to the XML folder', default='./json')
+parser.add_argument('-max', '--max_threads', default=60)
+args = parser.parse_args()
+
+# TODO: Use this variable to run multiple threads of 'transform_xml_to_json'
+MAX_THREADS = int(args.max_threads)
+
+
+def transform_xml_to_json(xml_file_input, json_file_output):
+    """Transform Single File"""
+    with open(xml_file_input,'r') as f1:
+        xml_text=f1.read()
+    with open(json_file_output,'w') as f2:
+        json_text=dumps(parker.data(fromstring((xml_text))))
+        f2.write(json_text)
+
+
+def process_folder(xml_folder, json_folder):
+    """For each file in xml_folder call transform_xml_to_json and
+    Create JSON file in json_folder"""
+    # TODO: To complete
+
+
+if __name__ == "__main__":
+    # TODO: Write a code to run transform_xml_to_json in multiple threads for different folders.
+    # This code should read the structure of input_xml_folder and replicate the structure in json_output_folder
+    # The code should use multiprocessing to spawn a threat for each sub folder up to MAX_THREADS
+    #
+
+    print('done')
